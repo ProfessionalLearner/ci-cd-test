6927749dfeff985002ca100adecadfa67bf67ac7
Arnur <arnur.nogaibekov@mail.utoronto.ca>
2020-12-25 08:16:18 +0600

    Fixed production env

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-if (process.env)
-app.use(express.static(__dirname + '/webapp/'))
-app.get(/.*/, (req, res) => {
-    res.sendFile(__dirname + '/webapp/index.html');
-});
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static(__dirname + '/webapp/'))
+    app.get(/.*/, (req, res) => {
+        res.sendFile(__dirname + '/webapp/index.html');
+    });
+}
 
 app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
 });
